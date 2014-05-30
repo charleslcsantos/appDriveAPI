27f038cca984a54cc658446efad8ff91f98d83d5
Charles Lennon <charleslcsantos@gmail.com>
2014-05-30 15:12:59 -0300

    implementando dashboard

--- app.js
+++ app.js
@@ -4,9 +4,11 @@ var favicon = require('static-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var dropbox = require("dropbox");
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var dashboard = require('./routes/dashboard');
 
 var app = express();
 
@@ -24,6 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/dashboard', dashboard);
 
 /// catch 404 and forward to error handler
 app.use(function(req, res, next) {
